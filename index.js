48704751d455f626130433a53ca66113d5a2935c
rikudi <rikukaartoaho@gmail.com>
2023-12-09 06:37:32 +0200

    3.22 ESLint+fix

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
+        return response.status(400).send({ error: 'malformatted id' })
     } else if(error.name === 'ValidationError') {
-        return response.status(400).send({error: error.message})
+        return response.status(400).send({ error: error.message })
     }
     next(error)
 }
@@ -21,7 +21,7 @@ const errorHandler = (error, request, response, next) => {
 app.use(cors())
 //initialize express
 app.use(express.json())
-app.use(express.static('dist'))
+app.use(express.static('build'))
 //create custom Morgan token for HTTP POST requests
 morgan.token('post-data', (request, response) => {
     return request.method === 'POST' ? JSON.stringify(request.body) : ''
@@ -31,11 +31,11 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 
 //Handle HTTP GET requests
 app.get('/api/persons', (request, response) => {
-    console.log("Request for persons data received from browser")
+    console.log('Request for persons data received from browser')
     Person.find({}).then(persons => {
         response.json(persons)
     })
-    console.log("persons: response sent from server")
+    console.log('persons: response sent from server')
 })
 
 //handle HTTP POST requests
@@ -52,43 +52,43 @@ app.post('/api/persons', (request, response, next) => {
         name: body.name,
         number: body.number
     })
-   //save newPerson to database and console.log
+    //save newPerson to database and console.log
     newPerson.save().then(savedPerson => {
         console.log(savedPerson)
         response.json(savedPerson)
     })
-    .catch(error => next(error))
+        .catch(error => next(error))
 })
 
 //handle requests for a single resource (person)
 //Use Person.findById method to fetch person data from Mongo if matching id is found
 app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
-    .then(person => {
-        if(person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+        .then(person => {
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 //HTTP PUT request handler
 //need to update front-end?
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
-    
+
     const person = {
         name: body.name,
         number: body.number
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
-    .then(updatedPerson => {
-        response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 //initialize error handler
@@ -109,12 +109,11 @@ app.get('/info', (request, response) => {
 app.delete('/api/persons/:id', (request, response, next) => {
     console.log(`delete: request for id ${request.params.id} received`)
     Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
 
 //determine port
 const PORT = process.env.PORT
