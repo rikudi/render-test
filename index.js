73fb71ccdb902b979080fb8d9dad36ccd44112a2
rikudi <rikukaartoaho@gmail.com>
2023-12-09 06:07:49 +0200

    3.21+gitignore

--- index.js
+++ index.js
@@ -1,49 +1,27 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
-//hardcoded server db
-let persons = [
-    {
-        id: 1,
-        name: "Geoffrey Morrison",
-        number: "045-623445"
-    },
-    {
-        id:2,
-        name: "Giorgios Hunter",
-        number: "045-623446"
-    },
-    {
-        id: 3,
-        name: "Muumi Peikko",
-        number: "010-526997"
-    },
-    {
-        id: 4,
-        name: "Nuuskis Muikkunen",
-        number: "0100-100"
-    },
-]
-//function that determines content for info page
-const info = () => {
-    const personCount = persons.length
-    const currentDate = new Date()
-    return(
-        `
-            <p>Phonebook has info for ${personCount} persons</p>
-            <p>${currentDate}</p>
-        `
-    )
+//setup errorHandler
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).send({error: error.message})
+    }
+    next(error)
 }
+
 //enable cross origin requests
 app.use(cors())
 //initialize express
 app.use(express.json())
-
 app.use(express.static('dist'))
-
 //create custom Morgan token for HTTP POST requests
 morgan.token('post-data', (request, response) => {
     return request.method === 'POST' ? JSON.stringify(request.body) : ''
@@ -51,26 +29,17 @@ morgan.token('post-data', (request, response) => {
 //initialize morgan
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
-
-
-/*function that generates a new random unique id for new person
-function keeps generating an id for as long as the function doesn't
-find already existing id */
-const generateId = () => {
-    let newId
-    do {
-        newId = Math.floor(Math.random() * 1000000)
-    } while (persons.some(p => p.id === newId))
-
-    return newId
-}
-//check if name exists in persons array. Returns a boolean
-const nameExists = (nameToCheck) => {
-    return persons.some(person => person.name === nameToCheck)
-} 
+//Handle HTTP GET requests
+app.get('/api/persons', (request, response) => {
+    console.log("Request for persons data received from browser")
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+    console.log("persons: response sent from server")
+})
 
 //handle HTTP POST requests
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     //if request body has no content, respond with error
     if(!body.name || !body.number) {
@@ -78,65 +47,77 @@ app.post('/api/persons', (request, response) => {
             error: 'name or number missing'
         })
     }
-    if(nameExists(body.name)) {
-        return response.status(400).json({
-            error: 'Name already exists'
-        })
-    }
-    //create a new person object with fresh generated id and concat
-    const person = {
+    //instance of Person model and assign it to a constant newPerson
+    const newPerson = new Person({
         name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-    console.log(person)
-    persons = persons.concat(person)
-
-    response.json(person)
-})
-
-
-app.get('/', (request, response) => {
-    response.send('<h1>Node express is cool Bro</h1>')
-})
-
-//handles requests for persons collection
-app.get('/api/persons', (request, response) => {
-    console.log("Request for persons data received")
-    response.send(persons)
-    console.log("persons: response sent")
+        number: body.number
+    })
+   //save newPerson to database and console.log
+    newPerson.save().then(savedPerson => {
+        console.log(savedPerson)
+        response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 //handle requests for a single resource (person)
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+//Use Person.findById method to fetch person data from Mongo if matching id is found
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
+
+//HTTP PUT request handler
+//need to update front-end?
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+//initialize error handler
+app.use(errorHandler)
 
 //handle info page request
 app.get('/info', (request, response) => {
-    console.log("info page request received")
-    response.send(info())
-    console.log("info response sent")
+    Person.countDocuments({}).then(count => {
+        const currentDate = new Date()
+        response.send(`
+            <p>Phonebook has info for ${count} persons</p>
+            <p>${currentDate}</p>
+        `)
+    })
 })
 
-//handle resource delete requests
-app.delete('/api/persons/:id', (request, response) => {
+//handle HTTP DELETE requests
+app.delete('/api/persons/:id', (request, response, next) => {
     console.log(`delete: request for id ${request.params.id} received`)
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-    console.log(`delete: response sent. ID ${id} deleted`)
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+
 //determine port
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ models/person.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+//url
+const url = process.env.MONGODB_URI 
+const phoneRegex = /^(\d{3}-\d{8})$/
+//schema for Person
+const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      minLength: 8,
+      required: true,
+      validate: {
+        validator: function(v) {
+          return phoneRegex.test(v)
+        },
+        message: props => `${props.value} is not a valid phone number`
+      }
+    },
+  })
+
+  //convert database data to json format + fix id's
+  personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+  })
+
+mongoose.set('strictQuery', false)
+//making connection request and waiting for it to be established before proceeding
+console.log('connecting to: mongo phonebook db')
+mongoose.connect(url)
+.then(() => {
+    console.log('MongoDB connection established')
+})
+.catch(error => {
+    console.log(error)
+})
+
+module.exports = mongoose.model('Person', personSchema)
